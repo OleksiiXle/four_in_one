5c611ce419a56607b9d632cf3b6f49987956b25f
oleksii <oleksii.xle69@gmail.com>
2020-12-10 10:29:03 +0200

    xgrid1

--- common/widgets/xgrid/Xgrid.php
+++ common/widgets/xgrid/Xgrid.php
@@ -139,12 +139,6 @@ class Xgrid extends GridView
                 //  'id' => 'uploadStartBtn',
             ]);
 
-            if (is_array($this->dataProvider->filterModel->filterContent)) {
-                $filterContent = implode(',', $this->dataProvider->filterModel->filterContent);
-            } else {
-                $filterContent = $this->dataProvider->filterModel->filterContent;
-            }
-
             if ($this->useActions) {
                 $actionsWithChecked = "
                            <select class='checkActionsSelect' onchange='actionWithChecked(this);'>
@@ -174,7 +168,6 @@ class Xgrid extends GridView
                             <span>' . $actionsWithChecked . '</span>
                         </div>
                         <div class="col-md-7" align="left" style="font-style: italic;">'
-                            . $filterContent
                      . '</div>
                         <div class="col-md-1" align="right">
                           ' . $filterButton . '
@@ -370,4 +363,76 @@ class Xgrid extends GridView
         $checkBox = '<input type="checkbox" id="row-check-' .  $key . '" class="row-check" data-id = "' . $key . '" onChange="checkRow(this);" ' . $checked . '>';
         return Html::tag('td', $checkBox);
     }
+
+    /**
+     * Renders the summary text.
+     */
+    public function renderSummary()
+    {
+        $count = $this->dataProvider->getCount();
+
+        if (isset($this->dataProvider->filterModel)) {
+            if (is_array($this->dataProvider->filterModel->filterContent)) {
+                $filterContent = implode(',', $this->dataProvider->filterModel->filterContent);
+            } else {
+                $filterContent = $this->dataProvider->filterModel->filterContent;
+            }
+        } else {
+            $filterContent = '';
+        }
+
+        if ($count <= 0) {
+            return $filterContent;
+        }
+        $summaryOptions = $this->summaryOptions;
+        $tag = ArrayHelper::remove($summaryOptions, 'tag', 'div');
+        if (($pagination = $this->dataProvider->getPagination()) !== false) {
+            $totalCount = $this->dataProvider->getTotalCount();
+            $begin = $pagination->getPage() * $pagination->pageSize + 1;
+            $end = $begin + $count - 1;
+            if ($begin > $end) {
+                $begin = $end;
+            }
+            $page = $pagination->getPage() + 1;
+            $pageCount = $pagination->pageCount;
+            if (($summaryContent = $this->summary) === null) {
+                $ret = Html::tag($tag, Yii::t('yii', 'Showing <b>{begin, number}-{end, number}</b> of <b>{totalCount, number}</b> {totalCount, plural, one{item} other{items}}.', [
+                    'begin' => $begin,
+                    'end' => $end,
+                    'count' => $count,
+                    'totalCount' => $totalCount,
+                    'page' => $page,
+                    'pageCount' => $pageCount,
+                ]), $summaryOptions) . ' ' . $filterContent;
+                return $ret;
+            }
+        } else {
+            $begin = $page = $pageCount = 1;
+            $end = $totalCount = $count;
+            if (($summaryContent = $this->summary) === null) {
+                $ret = Html::tag($tag, Yii::t('yii', 'Total <b>{count, number}</b> {count, plural, one{item} other{items}}.', [
+                    'begin' => $begin,
+                    'end' => $end,
+                    'count' => $count,
+                    'totalCount' => $totalCount,
+                    'page' => $page,
+                    'pageCount' => $pageCount,
+                ]), $summaryOptions) . ' ' . $filterContent;
+                return $ret;
+            }
+        }
+
+        $ret = Yii::$app->getI18n()->format($summaryContent, [
+            'begin' => $begin,
+            'end' => $end,
+            'count' => $count,
+            'totalCount' => $totalCount,
+            'page' => $page,
+            'pageCount' => $pageCount,
+        ], Yii::$app->language) . ' ' . $filterContent;
+
+
+        return $ret;
+    }
+
 }
